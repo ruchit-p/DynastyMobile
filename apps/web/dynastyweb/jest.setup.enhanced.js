4fe84e20a42151a325e6467142fa0524fa3d0d30
Ruchit Patel <56946935+ruchit-p@users.noreply.github.com>
2025-06-11 12:45:28 -0400

    test: mock next navigation

--- apps/web/dynastyweb/jest.setup.enhanced.js
+++ apps/web/dynastyweb/jest.setup.enhanced.js
@@ -11,6 +11,7 @@
 
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import React from 'react'
 
 // =============================================================================
 // ENVIRONMENT SETUP
